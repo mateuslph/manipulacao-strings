98faf01e8e754a059ad02f61495afe4d38390471
mateuslph <mlptche@gmail.com>
2021-09-04 22:32:32 -0300

    primeiro commit

---
+++ extrator_cep.py
@@ -0,0 +1,9 @@
+endereco = "Rua das Flores 72, apartamento 1002, Laranjeiras Rio de Janeiro, RJ, 23440-120"
+
+import re #regular expression -- RegEx
+
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
+busca = padrao.search(endereco) #Match
+if busca:
+    cep = busca.group()
+    print(cep)
---
+++ validador_url.py
@@ -0,0 +1,5 @@
+# https://www.bytebank.com.br/cambio
+
+import re
+
+url = 'https://www.bytebank.com.br/cambio'
